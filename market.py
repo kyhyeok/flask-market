18d368b85055b15b1575983bf113f8595abefcad
김윤혁 <kyhyeok@gmail.com>
2022-01-10 00:19:24 +0900

    Template Inheritance

--- market.py
+++ market.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/about')
+@app.route('/home')
 def home_page():
     return render_template('base.html')
 
